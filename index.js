4989b7b62ea0f2767888e153a08a0a917cbb04a7
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-04-10 17:41:49 +0300

    updated script

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ movieSearchForm.addEventListener("submit", (event) => {
 
         document.getElementById("add-to-watchList-btn").addEventListener("click", () => {
             window.localStorage.setItem(`${data.Title}`, JSON.stringify(data));
-            
+            window.location.reload()
         })
 
         movieSearchForm.reset()
